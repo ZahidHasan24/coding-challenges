94ae631f54170a00ca41400b14ba728c8e4c5cf2
Zahid Hasan <hzahid2999@gmail.com>
2024-06-13 18:56:23 +0600

    add own wc tools second step that takes cli option -l that outputs the number of lines in a file.

--- src/wc/index.js
+++ src/wc/index.js
@@ -7,23 +7,41 @@ const getByteCount = (filePath) => {
   return fileSizeInBytes;
 };
 
+const getLineCount = (filePath) => {
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const lines = fileContent.split("\n");
+  return lines.length;
+};
+
 const displayUsage = () => {
-  console.log("Usage: -c <file>");
+  console.log("Usage: -c|-l <file>");
 };
 
 const main = () => {
   const args = process.argv.slice(2);
-  if (args.length !== 2 || args[0] !== "-c") {
+
+  if (args.length !== 2 || (args[0] !== "-c" && args[0] !== "-l")) {
     displayUsage();
     process.exit(1);
   }
+
   const filePath = args[1];
+  const option = args[0];
+
   if (!fs.existsSync(filePath)) {
     console.error(`File not found: ${filePath}`);
     process.exit(1);
   }
-  const byteCount = getByteCount(filePath);
-  console.log(`${byteCount} ${path.basename(filePath)}`);
+
+  if (option === "-c") {
+    const byteCount = getByteCount(filePath);
+    console.log(`${byteCount} ${path.basename(filePath)}`);
+  } else if (option === "-l") {
+    const lineCount = getLineCount(filePath);
+    console.log(`${lineCount} ${path.basename(filePath)}`);
+  } else {
+    throw new Error("Invalid option");
+  }
 };
 
 main();
